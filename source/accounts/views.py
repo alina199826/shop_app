4f510eef77f1f63f94ad0a93821c7db9f50284a6
Alina <gartok.alina@gmail.com>
2024-01-19 17:53:06 +0600

    modify app

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,25 +1,19 @@
-
-from django.urls import reverse
-from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView
-from django.contrib.auth import login, get_user_model
-
 
 class RegisterView(CreateView):
-    model = get_user_model()
+    form_class = UserCreationForm
     template_name = 'create_user.html'
-
+    success_url = reverse_lazy('index')
 
     def form_valid(self, form):
+        response = super().form_valid(form)
         user = form.save()
         login(self.request, user)
-        return redirect(self.get_success_url())
-
+        return response
 
     def get_success_url(self):
-        next_url = self.request.GET.get('next')
-        if not next_url:
-            next_url = self.request.POST.get('next')
-        if not next_url:
-            next_url = reverse('index')
-        return next_url
+        next_url = self.request.GET.get('next') or self.request.POST.get('next')
+        return next_url or reverse('index')
